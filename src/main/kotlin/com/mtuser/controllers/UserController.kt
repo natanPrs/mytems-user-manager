7e4aa01416fc1382686d3ebc5b4c66eb39d2f68c
Natan Pires <natan.void@hotmail.com>
2025-04-01 12:22:41 -0300

    feat: add user controller

---
+++ src/main/kotlin/com/mtuser/controllers/UserController.kt
@@ -0,0 +1,36 @@
+package com.mtuser.controllers
+
+import com.mtuser.dtos.UserDto
+import com.mtuser.models.UserModel
+import com.mtuser.services.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController(private val userService: UserService) {
+
+    @PostMapping
+    fun createUser(@RequestBody userDto: UserDto): ResponseEntity<UserModel> {
+        println(userDto)
+        val newUser = userService.createUser(userDto)
+        return ResponseEntity(newUser, HttpStatus.CREATED)
+    }
+
+    @DeleteMapping("/{email}")
+    fun deleteUser(@PathVariable email: String): ResponseEntity<String> {
+        userService.deleteUser(email)
+        return ResponseEntity("User has been deleted", HttpStatus.OK)
+    }
+
+    @GetMapping
+    fun getAllUsers(): ResponseEntity<List<UserModel>> { return ResponseEntity(userService.getAllUsers(), HttpStatus.OK)
+    }
+}
