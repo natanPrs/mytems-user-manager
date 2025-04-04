4b0282c53ee5900503706d79c75f4c27bbff29cf
Natan Pires <natan.void@hotmail.com>
2025-04-04 14:11:42 -0300

    feat: add query to find items

--- src/main/kotlin/com/mtuser/repositories/ItemRepository.kt
+++ src/main/kotlin/com/mtuser/repositories/ItemRepository.kt
@@ -1,8 +1,14 @@
 package com.mtuser.repositories
 
 import com.mtuser.domain.inventory.ItemModel
+import com.mtuser.dtos.InventoryResponseDto
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface ItemRepository : JpaRepository<ItemModel, UUID> {
+
+    @Query("SELECT NEW com.mtuser.dtos.InventoryResponseDto(i.title, i.status) FROM ItemModel i WHERE i.userOwner.email = :email")
+    fun findTitlesByUserEmail(@Param("email") email: String): List<InventoryResponseDto>
 }
