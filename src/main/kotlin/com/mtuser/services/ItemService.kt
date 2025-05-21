3d51117e4e3a4b07a0758491a2c0bd05a0f18c7f
natanPrs <natan.void@proton.me>
2025-05-21 16:37:03 -0300

    feat: adding JSON sending logic

--- src/main/kotlin/com/mtuser/services/ItemService.kt
+++ src/main/kotlin/com/mtuser/services/ItemService.kt
@@ -2,31 +2,33 @@ package com.mtuser.services
 
 import com.mtuser.domain.inventory.ItemModel
 import com.mtuser.domain.inventory.ItemStatus
-import com.mtuser.dtos.AnnounceItemDto
+import com.mtuser.dtos.ItemAnnouncedDto
+import com.mtuser.dtos.ItemToAnnounceDto
 import com.mtuser.dtos.ItemDto
+import com.mtuser.mappers.toItemAnnouncedDto
 import com.mtuser.mappers.toItemEntity
 import com.mtuser.repositories.ItemRepository
 import com.mtuser.repositories.UserRepository
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
-import javax.print.PrintService
 
 @Service
 class ItemService(
     private val itemRepository: ItemRepository,
     private val userRepository: UserRepository,
-    private val kafkaTemplate: KafkaTemplate<String, String>) {
+    private val kafkaTemplate: KafkaTemplate<String, ItemAnnouncedDto>){
 
     fun createItem(itemDto: ItemDto): ItemModel {
         val newItem = itemDto.toItemEntity(userRepository)
         return itemRepository.save(newItem)
     }
 
-    fun announceItem(announceItemDto: AnnounceItemDto) : ItemModel{
-        val itemToAnnounce = itemRepository.findById(announceItemDto.itemId)
-            .orElseThrow { Exception("Item not found") }
+    fun announceItem(itemToAnnounceDto: ItemToAnnounceDto) : ItemModel{
+        val itemToAnnounce = itemRepository.findById(itemToAnnounceDto.itemId)
+            .also { userRepository.findUserByEmail(itemToAnnounceDto.sellerEmail) }
+            .orElseThrow { Exception("Item or user not found") }
 
-        kafkaTemplate.send("announceItem", "Ola, estou testando o meu kafka!")
+        kafkaTemplate.send("announceItem", itemToAnnounceDto.toItemAnnouncedDto(itemRepository))
 
         itemToAnnounce.status = ItemStatus.ANNOUNCED
 
