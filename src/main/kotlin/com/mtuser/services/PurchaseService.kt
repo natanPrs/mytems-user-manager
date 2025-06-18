967ae1a28c0874ba308d7fb3c5412caef93152f3
natanPrs <natan.void@proton.me>
2025-06-18 10:12:37 -0300

    feat: add complete transaction function

--- src/main/kotlin/com/mtuser/services/PurchaseService.kt
+++ src/main/kotlin/com/mtuser/services/PurchaseService.kt
@@ -1,15 +1,30 @@
 package com.mtuser.services
 
+import com.mtuser.dtos.EventUpdateUserDto
 import com.mtuser.dtos.PurchaseDto
 import com.mtuser.repositories.UserRepository
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class PurchaseService(
-    private val userRepository: UserRepository) {
+    private val userRepository: UserRepository,
+    private val userService: UserService,
+    private val itemService: ItemService) {
 
-    fun validateUsers(purchaseDto: PurchaseDto): Boolean{
-        return userRepository.existsById(purchaseDto.sellerId) &&
-                userRepository.existsById(purchaseDto.buyerId)
+    fun validateUsers(purchaseDto: PurchaseDto): UUID?{
+        val sellerId = userRepository.findById(purchaseDto.sellerId)
+            .orElseThrow { Exception("user not found") }.id
+        userRepository.existsById(purchaseDto.buyerId)
+
+        return sellerId
     }
+
+    fun completeTransaction(eventUpdateUserDto: EventUpdateUserDto){
+        userService.updateUsersBalance(eventUpdateUserDto.globalItemId, eventUpdateUserDto.buyerId, eventUpdateUserDto.amount)
+        itemService.updateItemOwner(eventUpdateUserDto.globalItemId, eventUpdateUserDto.buyerId)
+
+        println("Aeeeeww!!")
+    }
+
 }
