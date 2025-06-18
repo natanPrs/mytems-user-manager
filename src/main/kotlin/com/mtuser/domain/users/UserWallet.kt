e2c38530c688d919c9205c1aeb74a43041dbac5b
natanPrs <natan.void@proton.me>
2025-06-18 10:14:09 -0300

    feat: add addMoney & removeMoney to wallet

--- src/main/kotlin/com/mtuser/domain/users/UserWallet.kt
+++ src/main/kotlin/com/mtuser/domain/users/UserWallet.kt
@@ -7,4 +7,12 @@ import java.math.BigDecimal
 data class UserWallet(
 
     var amount: BigDecimal = BigDecimal.ZERO
-)
+){
+    fun addMoney(amount: BigDecimal){
+        this.amount.add(amount)
+    }
+
+    fun removeMoney(amount: BigDecimal){
+        this.amount.subtract(amount)
+    }
+}
